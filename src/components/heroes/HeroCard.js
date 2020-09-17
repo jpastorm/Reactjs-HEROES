6b14d7d0efa0c829fc22d6f54c9c602a76aa2d3a
jpastorm <jospastorm@upt.pe>
2020-09-16 23:56:24 -0500

    Reactjs HEROES

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -1,41 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 export const HeroCard = ({
-	id,
-	superhero,
-	publisher,
-	alter_ego,
-	first_appearance,
-	characters,
+  id,
+  superhero,
+  publisher,
+  alter_ego,
+  first_appearance,
+  characters,
 }) => {
-	return (
-		<div className="card ms-3" style={{ maxWidth: 540 }}>
-			<div className="row no-gutters">
-				<div className="col-md-4">
-					<img
-						src={`./assets/heroes/${id}.jpg`}
-						className="card-img"
-						alt={superhero}
-					/>
-				</div>
-				<div className="col-md-8">
-					<div className="card-body">
-						<h5 className="card-title">{superhero}</h5>
-						<div className="card-text">
-							{alter_ego}
-							{alter_ego != characters && (
-								<p className="card-text">{characters}</p>
-							)}
-							<p className="card-text">
-								<small className="text-muted">
-									{first_appearance}
-								</small>
-							</p>
-							<Link to={`./hero/${id}`}>Más ..</Link>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="card ms-3" style={{ maxWidth: 540 }}>
+      <div className="row no-gutters">
+        <div className="col-md-4">
+          <img
+            src={`./assets/heroes/${id}.jpg`}
+            className="card-img"
+            alt={superhero}
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">{superhero}</h5>
+            <div className="card-text">
+              {alter_ego}
+              {alter_ego !== characters && (
+                <p className="card-text">{characters}</p>
+              )}
+              <p className="card-text">
+                <small className="text-muted">{first_appearance}</small>
+              </p>
+              <Link to={`./hero/${id}`}>Más ..</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -6,63 +6,61 @@ import { useForm } from "../../hooks/useForm";
 import { useLocation } from "react-router-dom";
 import { getHeroesByName } from "../../selectors/getHeroesByName.js";
 export const SearchScreen = ({ history }) => {
-	const location = useLocation();
+  const location = useLocation();
 
-	const { q = "" } = queryString.parse(location.search);
+  const { q = "" } = queryString.parse(location.search);
 
-	const [formValues, handleInputChange] = useForm({
-		searchText: q,
-	});
-	const { searchText } = formValues;
-	const heroesFiltered = useMemo(() => getHeroesByName(q), [q]);
+  const [formValues, handleInputChange] = useForm({
+    searchText: q,
+  });
+  const { searchText } = formValues;
+  const heroesFiltered = useMemo(() => getHeroesByName(q), [q]);
 
-	const handleSearch = (e) => {
-		e.preventDefault();
-		console.log(searchText);
-		history.push(`?q=${searchText}`);
-	};
-	return (
-		<div>
-			<h1>Search Screen</h1>
-			<hr />
-			<div className="row">
-				<div className="col-5">
-					<h4>Search Form</h4>
-					<hr />
-					<form onSubmit={handleSearch}>
-						<input
-							type="text"
-							placeholder="Find your hero"
-							className="form-control"
-							name="searchText"
-							autoComplete="off"
-							value={searchText}
-							onChange={handleInputChange}
-						/>
-						<button
-							className="btn mt-1 btn-block btn-outline-primary"
-							type="submit"
-						>
-							Search ..
-						</button>
-					</form>
-				</div>
-				<div className="col-7">
-					<h4>Results</h4>
-					<hr />
-					{q === "" && (
-						<div className="alert alert-info">Search a hero..</div>
-					)}
-					{q != "" && heroesFiltered.length === 0 && (
-						<div className="alert alert-danger">
-							There is no a hero with {q}{" "}
-						</div>
-					)}
-					{heroesFiltered.map((hero) => (
-						<HeroCard key={hero.id} {...hero} />
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(searchText);
+    history.push(`?q=${searchText}`);
+  };
+  return (
+    <div>
+      <h1>Search Screen</h1>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Search Form</h4>
+          <hr />
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              placeholder="Find your hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={handleInputChange}
+            />
+            <button
+              className="btn mt-1 btn-block btn-outline-primary"
+              type="submit"
+            >
+              Search ..
+            </button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+          {q === "" && <div className="alert alert-info">Search a hero..</div>}
+          {q !== "" && heroesFiltered.length === 0 && (
+            <div className="alert alert-danger">
+              There is no a hero with {q}{" "}
+            </div>
+          )}
+          {heroesFiltered.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
